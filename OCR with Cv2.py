90992960c399f39a4a84b70b7a497c7f2ddb1a42
Lucas-Esteves <116310896+Lucas-Esteves@users.noreply.github.com>
2022-12-29 16:47:38 -0300

    Update OCR with Cv2.py

--- OCR with Cv2.py
+++ OCR with Cv2.py
@@ -12,6 +12,6 @@ cv2.waitKey(0) #Para mostrar la imagen, se puede sacar
 ROI = Image [280:380, 40:200] 
 cv2.imshow("Facturar A", ROI) #Para mostrar la imagen, se puede sacar
 cv2.waitKey(0) #Para mostrar la imagen, se puede sacar
-cv2.imwrite(r'C:\Users\estev\Desktop\Prueba Tesseract\ROI1.PNG', ROI) #Guardar una imagen que esta en una variable
+cv2.imwrite(r'C:\Users\estev\Desktop\Prueba Tesseract\ROI1.PNG', ROI) #Guardar una imagen que esta en una variable (Lugar donde se guarda, variable)
 
 print (pytesseract.image_to_string(r'C:\Users\estev\Desktop\Prueba Tesseract\ROI1.PNG'))
