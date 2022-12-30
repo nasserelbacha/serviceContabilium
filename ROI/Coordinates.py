94615565fad77a7ebd06d13a044cd6efa3e6ee25
Lucas-Esteves <esteveslucas52@gmail.com>
2022-12-30 17:28:26 -0300

    files

---
+++ ROI/Coordinates.py
@@ -0,0 +1,37 @@
+import pytesseract
+import cv2
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
+image = cv2.imread((r'C:\Users\estev\Desktop\Prueba Tesseract\Codigos\serviceContabilium\Factura A.PNG')) #Asignarle una imagen a una variable
+
+coordinates = [] 
+  
+def shape_selection(event, x, y, flags, param): 
+    global coordinates 
+    if event == cv2.EVENT_LBUTTONDOWN: 
+        coordinates = [(x, y)] 
+    elif event == cv2.EVENT_LBUTTONUP: 
+        coordinates.append((x, y)) 
+        cv2.rectangle(image, coordinates[0], coordinates[1], (0,255,255), 2) 
+        cv2.imshow("image", image)   
+image_copy = image.copy()
+cv2.namedWindow("image") 
+cv2.setMouseCallback("image", shape_selection)  
+while True: 
+    cv2.imshow("image", image) 
+    key = cv2.waitKey(1) & 0xFF
+    if key==13: # If 'enter' is pressed, apply OCR
+        break
+    if key == ord("c"): # Clear the selection when 'c' is pressed 
+        image = image_copy.copy()  
+if len(coordinates) == 2: 
+    image_roi = image_copy[coordinates[0][1]:coordinates[1][1], 
+                               coordinates[0][0]:coordinates[1][0]] 
+    cv2.imshow("Selected Region of Interest - Press any key to proceed", image_roi) 
+    cv2.waitKey(0) 
+  
+cv2.destroyAllWindows()  
+    
+text = pytesseract.image_to_string(image_roi)
+print("The text in the selected region is as follows:")
+print(text)
+
--- app_contabilium/api/api_contabilium/tests/OCR with Cv2.py
+++ app_contabilium/api/api_contabilium/tests/OCR with Cv2.py
@@ -3,7 +3,7 @@ import cv2
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
 
 def readimage():
-    Image = cv2.imread((r'C:\Users\estev\Desktop\Prueba Tesseract\Factura A.PNG')) #Asignarle una imagen a una variable
+    Image = cv2.imread((r'C:\Users\estev\Desktop\Prueba Tesseract\Codigos\serviceContabilium\Factura A.PNG')) #Asignarle una imagen a una variable
 
     #Para mostrar la imagen, se puede sacar
     #cv2.imshow("Imagen", Image) 
@@ -14,8 +14,22 @@ def readimage():
     #cv2.imshow("Facturar A", ROI) #Para mostrar la imagen, se puede sacar
     #cv2.waitKey(0) #Para mostrar la imagen, se puede sacar
 
-    ROI = Image [280:380, 40:200] 
-    print (pytesseract.image_to_string(ROI))
+    facturar = Image [280:380, 40:200] 
+    print (pytesseract.image_to_string(facturar))
+    enviar = Image [280:380 , 290:425]
+    print (pytesseract.image_to_string(enviar))
+    fecha = Image[200:225, 620:710]
+    print (pytesseract.image_to_string(fecha))
+    subtotal = Image [573:601, 651:711]
+    print (pytesseract.image_to_string(subtotal))
+    iva = Image [610:630, 650:710]
+    print (pytesseract.image_to_string(iva))
+    total = Image [655:700, 570:670]
+    print (pytesseract.image_to_string(total))
+    nfactura = Image [170:197, 640:710]
+    print (pytesseract.image_to_string(nfactura))
+    
+    
 
 #cv2.imwrite(r'C:\Users\estev\Desktop\Prueba Tesseract\ROI1.PNG', ROI) #Guardar una imagen que esta en una variable
 #print (pytesseract.image_to_string(r'C:\Users\estev\Desktop\Prueba Tesseract\ROI1.PNG'))
@@ -25,4 +39,3 @@ def main():
 
 main()
 
-#test
