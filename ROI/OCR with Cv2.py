de0b125c1a8a0b7e8e18c34454d56aa1d4a0d285
Lucas-Esteves <esteveslucas52@gmail.com>
2023-01-03 16:40:07 -0300

    files

--- ROI/OCR with Cv2.py
+++ ROI/OCR with Cv2.py
@@ -2,6 +2,7 @@ import pytesseract
 import cv2
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
 
+
 def readimage():
     Image = cv2.imread((r'C:\Users\estev\Desktop\Prueba Tesseract\Codigos\serviceContabilium\Factura A.PNG')) #Asignarle una imagen a una variable
 
@@ -39,3 +40,11 @@ def main():
 
 main()
 
+coordinates = [] 
+def shape_selection(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN: 
+        coordinates = [(x, y)] 
+    elif event == cv2.EVENT_LBUTTONUP: 
+        coordinates.append((x, y)) 
+    cv2.rectangle(image, coordinates[0], coordinates[1], (0,255,255), 2) 
+    cv2.imshow("image", image) 
