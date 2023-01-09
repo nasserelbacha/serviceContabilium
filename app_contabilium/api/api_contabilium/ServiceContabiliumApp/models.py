8864ef57ae5428698ff7c65aa02b673757d4bee6
Nasser <nasseramelbacha2002@gmail.com>
2023-01-09 19:41:22 -0300

    typedoc and company working

--- app_contabilium/api/api_contabilium/ServiceContabiliumApp/models.py
+++ app_contabilium/api/api_contabilium/ServiceContabiliumApp/models.py
@@ -5,38 +5,37 @@ import uuid
 # Create your models here.
 
 class Companies(models.Model):
-    # id = models.UUIDField(primary_key=True,default=uuid.uuid4, editable=False, unique=True)
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4, editable=False, unique=True)
     email = models.EmailField(max_length=233, default='hola@gmail.com')
     password = models.CharField(max_length=233, default='')
-#    employees = models.ForeignKey(Employees, on_delete=models.CASCADE)
 
 class Employees(models.Model):
-    # id = models.UUIDField(primary_key=True,default=uuid.uuid4, editable=False, unique=True)
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4, editable=False, unique=True)
     name = models.CharField(max_length=233, default='')
-    lastNamess = models.CharField(max_length=233, default='')
+    lastNames = models.CharField(max_length=233, default='')
     email = models.EmailField(max_length=233, default='hola@gmail.com')
     password = models.CharField(max_length=233, default='')
     isAdmin = models.BooleanField(default=False)
-    company = models.OneToOneField(Companies, on_delete= models.CASCADE, default=1)
+    company = models.ForeignKey(Companies, on_delete= models.CASCADE)
 
 class Providers(models.Model):
-    id = models.UUIDField(primary_key=True,default=uuid.uuid4, editable=False, unique=True)
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4, editable=False, unique=True,)
     name = models.CharField(max_length=233, default='')
-#    company= models.OneToOneField(Companies, on_delete=models.CASCADE)
-#    provider_config = models.ForeignKey(Bill, on_delete= models.CASCADE)
+    company= models.ForeignKey(Companies, on_delete=models.CASCADE)
+    # provider_config = models.ForeignKey(Bill, on_delete= models.CASCADE)
 
 class TypeDoc(models.Model):
-    # id = models.UUIDField(primary_key=True,default=uuid.uuid4, editable=False, unique=True,)
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4, editable=False, unique=True,)
     name = models.CharField(max_length=255, null=True)
 
 class Bill(models.Model):
     id = models.UUIDField(primary_key=True,default=uuid.uuid4, editable=False, unique=True)
-    typedoc = models.ForeignKey(TypeDoc, on_delete= models.CASCADE, default=1)
+    typedoc = models.ForeignKey(TypeDoc, on_delete= models.CASCADE)
     name = models.CharField(max_length=233, default='')
     coordate_x = models.IntegerField(default=1)
     coordate_y = models.IntegerField(default=1)
-    provider = models.ForeignKey(Providers, on_delete= models.CASCADE, default=1)
-    company = models.ForeignKey(Companies, on_delete= models.CASCADE, default=1)
+    provider = models.ForeignKey(Providers, on_delete= models.CASCADE)
+    company = models.ForeignKey(Companies, on_delete= models.CASCADE)
     info = models.CharField(max_length=233, default='')
 
     
--- app_contabilium/api/api_contabilium/ServiceContabiliumApp/urls.py
+++ app_contabilium/api/api_contabilium/ServiceContabiliumApp/urls.py
@@ -7,8 +7,9 @@ from django.conf import settings
 
 urlpatterns = [
     path('typesDoc/', TypeDocView.as_view(), name='type_doc_list'),
-    path('typesDoc/<int:id>', TypeDocView.as_view(), name='type_doc_process'),
-    path('companies/', CompaniesView.as_view(), name='companies_list'),
-    path('companies/<int:id>', CompaniesView.as_view(), name='companies_process'),
-    path('companies/employees', EmployeesView.as_view(), name='employees_list'),
+    path('typesDoc/<str:id>', TypeDocView.as_view(), name='type_doc_process'),
+    path('companies', CompaniesView.as_view(), name='companies_list'),
+    path('companies/<str:id>', CompaniesView.as_view(), name='companies_process'),
+    path('companies/<str:id>/employees', EmployeesView.as_view(), name='employees_list'),
+    path('companies/employees', EmployeesView.as_view(), name='employees_process'),
 ]
